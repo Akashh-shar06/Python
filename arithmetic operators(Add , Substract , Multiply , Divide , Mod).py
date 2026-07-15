4859740afda270b3067c236e6a2a5a2047db4b9b
Aakash Sharma <akashshar9029@gmail.com>
2026-07-15 18:37:58 +0530

    Create arithmetic operators(Add , Substract , Multiply , Divide , Mod).py

---
+++ arithmetic operators(Add , Substract , Multiply , Divide , Mod).py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter the Value : "))
+num2 = int(input("Enter the Value : "))
+
+print(f"{num1} + {num2} = {num1+num2}")
+print(f"{num1} - {num2} = {num1-num2}")
+print(f"{num1} * {num2} = {num1*num2}")
+print(f"{num1} / {num2} = {num1/num2}")
+print(f"{num1} % {num2} = {num1%num2}")
+
+print(num1," + ", num2 ," = ",(num1+num2))
+print(num1," - ", num2 ," = ",(num1-num2))
+print(num1," * ", num2 ," = ",(num1*num2))
+print(num1," / ", num2 ," = ",(num1/num2))
+print(num1," % ", num2 ," = ",(num1%num2))
